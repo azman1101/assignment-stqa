1e7aa2a734278df46b84844700524231dfa70be6
Azman <33550114+azman1101@users.noreply.github.com>
2020-02-28 17:34:23 +0800

    Update 6_getPatientData.py

--- 6_getPatientData.py
+++ 6_getPatientData.py
@@ -14,35 +14,24 @@ daySpent = -1
 medCharge = -1
 hospServCharge = -1
 
-'''
-while (num < 0):
-# this mean program do not tolerate negative input,
-# it will prompt user until non negative num is enter
-'''
-
-# function
 def getPatientData():
     global patient
     
     patient["name"] = input("Enter patient name: ")
     patient["IC"] = input("Enter patient IC: ")
-    #patient["name"] = "Mohd"
-    #patient["IC"] = "9502"
     
     print("\nPlease choose patient admission type:")
     print("1. In-patient")
     print("2. Out-patient")
     patient["admission"] = input("\nAdmission type:")
-    
+
     return patient["admission"]
 
-# function
 def calculateInPatientCharges(daySpent):
     global medCharge, hospServCharge
     total = dailyRate * daySpent + medCharge + hospServCharge
     return total
 
-# function
 def calculateOutPatientCharges():    
     global medCharge, hospServCharge
     total = medCharge + hospServCharge
@@ -56,7 +45,6 @@ if (admissionType == 1):
     while (daySpent < 0):
         daySpent = int(input("How many day spent: "))
 
-    # both Out & In will use these
 while (medCharge < 0):
     medCharge = int(input("Hospital medication charges: "))
 
